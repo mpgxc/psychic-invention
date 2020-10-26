09826b4d4e33d4c9a9727d672a4909b18bb8bd12
mpgxc <mpgx5.c@gmail.com>
2020-10-26 13:33:47 -0300

    chore: add call scrap untion to tests

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import getPalleteColors from "./getPalleteColors";
 import readImageToBuffer from "./readImageToBuffer";
+import ScrapPage from "./utils/createScrapPage";
 
 const image = readImageToBuffer("./tmp/person.jpg");
 
 getPalleteColors(image, "jpg").then(console.log);
+
+ScrapPage().then();
