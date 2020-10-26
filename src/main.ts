18edbe988517661d2bc18aef9b48aac311524409
mpgxc <mpgx5.c@gmail.com>
2020-10-26 14:58:00 -0300

    feat: add similarity colorswith distance

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import getPalleteColors from "./getPalleteColors";
 import readImageToBuffer from "./readImageToBuffer";
-import ScrapPage from "./services/createScrapPage.Service";
+import createScrapPage from "./services/createScrapPage.Service";
+import createColorSimilarity from "./services/createColorSimilarity.Service";
 
 const image = readImageToBuffer("./tmp/person.jpg");
 
 getPalleteColors(image, "jpg").then(console.log);
 
-ScrapPage().then();
+// createScrapPage().then();
+
+const resultMatch = createColorSimilarity("#673524", "#955135");
+
+console.log("Delta: ", resultMatch.deltae);
+console.log("Euclidian: ", resultMatch.euclidian);
--- src/services/createColorSimilarity.Service.ts
+++ src/services/createColorSimilarity.Service.ts
@@ -1,1 +1,10 @@
-console.log("Vai dar tudo certo!");
+import { deltaE, distance } from "chroma-js";
+
+//ref: https://gka.github.io/chroma.js/#chroma-distance
+
+export default (colorA: string, colorB: string) => {
+  return {
+    deltae: deltaE(colorA, colorB),
+    euclidian: distance(colorA, colorB, "rgb"),
+  };
+};
